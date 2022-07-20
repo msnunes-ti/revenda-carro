6ceb9ed5b3cfc5dd168668253b379cb58f867878
Marcelo da Silva Nunes <msnunes.ti@gmail.com>
2022-07-20 17:29:08 -0300

    Inicio Revenda Carro

---
+++ src/main/java/com/example/revendacarro/RevendaCarroApplication.java
@@ -0,0 +1,13 @@
+package com.example.revendacarro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevendaCarroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevendaCarroApplication.class, args);
+	}
+
+}
