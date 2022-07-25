b339d98deff5c9727f8256ad9e93218f3e107d81
Marcelo da Silva Nunes <msnunes.ti@gmail.com>
2022-07-24 23:56:59 -0300

    atualizado

--- src/main/java/com/example/revendacarro/model/Veiculo.java
+++ src/main/java/com/example/revendacarro/model/Veiculo.java
@@ -1,4 +1,13 @@
 package com.example.revendacarro.model;
 
 public class Veiculo {
+
+    private Long id;
+
+    private String modelo;
+
+    private String placa;
+
+    private Proprietario proprietario;
+
 }
